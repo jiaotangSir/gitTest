a518316568b4356aa8de311cade3baaf5bb79bf7
shirley <shirley@hotmail.com>
2015-12-29 15:28:42 +0800

    gitTest2

---
+++ gitTest2/gitTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest2
+//
+//  Created by tarena6 on 15/12/29.
+//  Copyright © 2015年 tarena6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest2/gitTest2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest2
+//
+//  Created by tarena6 on 15/12/29.
+//  Copyright © 2015年 tarena6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
